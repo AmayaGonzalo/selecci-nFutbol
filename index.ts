7dcfeeb75cff46c423af399fa536e9bb814f5cf1
Amaya Gonzalo Javier <103613393+AmayaGonzalo@users.noreply.github.com>
2022-10-14 19:54:56 -0300

    ultimo commit

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ let FutbolistaUno: Futbolista = new Futbolista ("Lionel", "Messi", 35, 10, 21, 1
 let FutbolistaDos: Futbolista = new Futbolista ("Lautaro", "Martinez", 25, 22, 21, 11);
 let FutbolistaTres: Futbolista = new Futbolista ("Franco", "Armani", 35, 1, 21, 11)
 
+//ejecucion del programa
 let nuevaListaJugadores = [FutbolistaUno, FutbolistaDos]
 
 let MasajistaUno: Masajista = new Masajista ("Juan", "Perez,", 30, nuevaListaJugadores);
