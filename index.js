6827f1de69dd5bdb1f2d4d5a8db03a604fc11321
Amaya Gonzalo Javier <103613393+AmayaGonzalo@users.noreply.github.com>
2022-10-14 19:55:36 -0300

    ultimo commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var Masajista_1 = require("./Masajista");
 var FutbolistaUno = new Futbolista_1.Futbolista("Lionel", "Messi", 35, 10, 21, 11);
 var FutbolistaDos = new Futbolista_1.Futbolista("Lautaro", "Martinez", 25, 22, 21, 11);
 var FutbolistaTres = new Futbolista_1.Futbolista("Franco", "Armani", 35, 1, 21, 11);
+//ejecucion del programa
 var nuevaListaJugadores = [FutbolistaUno, FutbolistaDos];
 var MasajistaUno = new Masajista_1.Masajista("Juan", "Perez,", 30, nuevaListaJugadores);
 MasajistaUno.AsignarJugador(FutbolistaTres);
